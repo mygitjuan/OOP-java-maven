ab1b0e4be0b4bbafaacf5f3b285f1d107029fc6b
mygitjuan <jarellano.aguirre@gmail.com>
2023-10-18 01:50:37 +0200

    actualizar clases

--- ShoppingCart/Product.java
+++ ShoppingCart/Product.java
@@ -1,20 +1,20 @@
 public class Product {
 	private String title;
 	private double price;
-	
+
 	public Product (String t, double p) {
 		this.title = t;
 		this.price = p;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public boolean equals(Object o) {
 		if (o instanceof Product) {
 			Product p = (Product)o;
--- ShoppingCart/ShoppingCart.java
+++ ShoppingCart/ShoppingCart.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class ShoppingCart {
-	
+
 	private ArrayList items;
-	
+
 	public ShoppingCart() {
 		items = new ArrayList();
 	}
-	
+
 	public double getBalance() {
 		double balance = 0.00;
 		for (Iterator i = items.iterator(); i.hasNext();){
@@ -16,23 +16,22 @@ public class ShoppingCart {
 		}
 		return balance;
 	}
-	
+
 	public void addItem(Product item) {
 		items.add(item);
 	}
-	
+
 	public void removeItem(Product item)
 			throws ProductNotFoundException {
 		if (!items.remove(item)) {
 			throw new ProductNotFoundException();}
 	}
-	
+
 	public int getItemCount() {
 		return items.size();
 	}
-	
+
 	public void empty() {
 		items.clear();
 	}
 }
-

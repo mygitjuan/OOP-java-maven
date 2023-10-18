c81834aee4b068aaf389c572c76e7c0a44ab1087
mygitjuan <jarellano.aguirre@gmail.com>
2023-10-18 02:32:05 +0200

    añadir MAVEN

---
+++ ShoppingCart/src/main/java/com/DXC/MyShoppingCart/App.java
@@ -0,0 +1,13 @@
+package com.DXC.MyShoppingCart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ShoppingCart/src/main/java/com/DXC/MyShoppingCart/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.DXC.MyShoppingCart;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException() {
+        super();
+    }
+}
